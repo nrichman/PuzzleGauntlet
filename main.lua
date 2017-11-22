55136f19b5a7bdd86a6679ffb424e7911911bdd1
NICK <nrichman962@gmail.com>
2017-11-21 21:07:17 -0600

    Added StateMachine from Hump. Added temp graphics and RPGMaker graphics.

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ require 'board'
 require 'pieces'
 require 'mouseinput'
 
+local gamestate = {}
+local menustate = {}
+
+StateMachine = require "StateMachine"
 --Initialize our board object
 board = love.graphics.newImage('resources/board.png')
 boardstate = Board:new()
@@ -10,19 +14,13 @@ boardstate = Board:new()
 turn = 'x'
 winner = ''
 
+
 --Load method occurs first
 function love.load()
     love.filesystem.load('board.lua')()
     love.filesystem.load('mouseinput.lua')()
-end
-
---Draw method occurs every frame
-function love.draw()
-    love.graphics.setColor(255,255,255,255); --Sets color to white 
-
-    imageScaleX,imageScaleY = getScaling(board)
-    love.graphics.draw(board,0,0,0,imageScaleX,imageScaleY) --Draws the board
-    boardstate:draw() --Draws the pieces on the board
+    StateMachine.registerEvents()
+    StateMachine.switch(menustate)
 end
 
 --Method occurs when mouse is pressed
@@ -39,14 +37,23 @@ function love.mousepressed(x, y, button, istouch)
     end
 end
 
-function love.keypressed(key)
-    if key == "escape" then
-        love.event.quit()
-    end
+function menustate:enter()
+    
+end
+    
+function menustate:update(dt)
+    
+end
+    
+function menustate:draw()
+    
+end
 
-    if key == "space" then
-        winner = ''
-        boardstate:clear()
-        turn = 'x'
-    end
+--Draw method occurs every frame
+function gamestate:draw()
+    love.graphics.setColor(255,255,255,255); --Sets color to white 
+
+    imageScaleX,imageScaleY = getScaling(board)
+    love.graphics.draw(board,0,0,0,imageScaleX,imageScaleY) --Draws the board
+    boardstate:draw() --Draws the pieces on the board
 end
